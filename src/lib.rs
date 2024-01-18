1b0f75bf5a8bb3edfba885c8a8bdcf60505f29c4
Jay Nagin <jknagin@gmail.com>
2024-01-17 17:49:46 -0800

    Add test for k>N

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,13 @@ mod tests {
             vec![&1, &2, &3],
             top_k_most_frequent(&[1, 1, 1, 2, 2, 3], 6)
         );
+    }
 
+    #[test]
+    fn test_k_greater_than_n() {
+        assert_eq!(
+            vec![&1, &2, &3],
+            top_k_most_frequent(&[1, 1, 1, 2, 2, 3], 7)
+        );
     }
 }
